e911d0a2d411d5db6a9ec193e4e8aa51960a6cd7
ammarAlzaqam <git@ammaralzaqam.com>
2025-11-03 02:31:17 +0200

    fix up button go

--- components/shared/FixedUpButton.tsx
+++ components/shared/FixedUpButton.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import { FaAngleDoubleUp } from "react-icons/fa";
 
 export default function FixedUpButton() {
-  const router = useRouter();
   const [isVisible, setIsVisible] = useState(false);
   const [animateArrow, setAnimateArrow] = useState(false);
 
@@ -18,26 +16,27 @@ export default function FixedUpButton() {
 
   const handleClick = () => {
     setAnimateArrow(true);
-    router.push("#home");
-    setTimeout(() => setAnimateArrow(false), 600); // نفس مدة الأنيميشن
+    setTimeout(() => setAnimateArrow(false), 650); // نفس مدة الأنيميشن
   };
 
   return (
-    <div
-      className={`fixed bottom-5 right-5 sm:hidden transition-opacity duration-500 z-[100] ${
-        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
-    >
-      <button
-        onClick={handleClick}
-        className="relative overflow-hidden cursor-pointer bg-primary-700 rounded-full p-3 hover:scale-110 transition-transform"
+    <a href="#home" className="fixed bottom-4 right-4 z-100">
+      <div
+        className={`sm:hidden transition-opacity duration-500 ${
+          isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
       >
-        <FaAngleDoubleUp
-          className={`text-primary-100 size-9 transition-transform ${
-            animateArrow ? "animate-arrow-bounce" : ""
-          }`}
-        />
-      </button>
-    </div>
+        <button
+          onClick={handleClick}
+          className="relative overflow-hidden cursor-pointer bg-primary-700 rounded-full p-3 hover:scale-110 transition-transform"
+        >
+          <FaAngleDoubleUp
+            className={`text-primary-100 size-9 transition-transform ${
+              animateArrow ? "animate-arrow-bounce" : ""
+            }`}
+          />
+        </button>
+      </div>
+    </a>
   );
 }
